49b7d05643aa048df6e04851b90facba46d10382
Jorge Luis Rangel Peralta <georgeranpe@gmail.com>
2019-12-28 11:23:26 -0600

    Better syntax
    In this function the code is equivalent but much simpler which is good

--- src/graphql/services/getAuthors.js
+++ src/graphql/services/getAuthors.js
@@ -1,12 +1,8 @@
 import { connect, disconnect } from '../db';
 
-export default () => new Promise((resolve, reject) => (async () => {
-  try {
-    const bd = await connect();
-    const result = await bd.request().query('select TOP 100 * from authors');
-    disconnect();
-    return resolve(result.recordset);
-  } catch (e) {
-    return reject(e);
-  }
-})());
+export default async () => {
+  const bd = await connect();
+  const result = await bd.request().query('select TOP 100 * from authors');
+  disconnect();
+  return resolve(result.recordset);
+}
